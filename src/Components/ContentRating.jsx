c1726cc1d219203786755df76cc54b0e6dcbf24d
hadeel <ddjobs2021@gmail.com>
2024-09-17 07:53:29 -0400

    1

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,11 +5,41 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+    this.state = {
+        likes: 0,
+        dislikes: 0,
+        totalRatings: 0,
+      handleLike:() => {
+        this.setState((prevState) => ({
+            likes: prevState.likes + 1,
+            totalRatings: prevState.totalRatings + 1
+          }));
+      },
+      handleDislike:() => {
+        this.setState((prevState) => ({
+            dislikes: prevState.dislikes + 1,
+            totalRatings: prevState.totalRatings + 1
+          }));
+      }
+      };
   }
   render() {
     return (
      <>
-     <h1>Text Content Rating</h1>
+     <h1>Using AI in healthcare system</h1>
+     <div className='content-rating'>
+        <p>AI is transforming healthcare, offering potential benefits like faster diagnoses and personalized treatments. However, some concerns remain, such as data privacy and the risk of errors. What are your thoughts on the use of AI in healthcare?</p>
+        <div className='rating-buttons'>
+        <button className="like-button" onClick={this.state.handleLike}>
+            Like ({this.state.likes})
+          </button>
+          <button className="dislike-button" onClick={this.state.handleDislike}>
+            Dislike ({this.state.dislikes})
+          </button>
+          <p>Total Ratings: {this.state.totalRatings}</p>
+        </div>
+
+     </div>
      </>
     );
   }
